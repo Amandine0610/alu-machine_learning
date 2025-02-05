714a72438d163272647a5c652385b259d209649f
Amandine0610 <a.irakoze@gmail.com>
2025-02-05 17:44:33 +0200

    add

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def  definiteness(matrix):
+def definiteness(matrix):
+
+
     # Check if matrix is a numpy.ndarray
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
