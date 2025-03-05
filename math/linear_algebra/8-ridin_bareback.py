6bd66bb15b9d83835425c8be23e33a554abf22bb
Amandine0610 <a.irakoze@gmail.com>
2025-03-05 23:44:36 +0200

    initial commit

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-""" matrix multiplication
-"""
+"""Matrix multiplication."""
 
 
 def mat_mul(mat1, mat2):
-    """Performs matrix multiplication between mat1 and mat2"""
-    if len(mat1[0]) != len(mat2):# Check if matrices can be multiplied
+    """Performs matrix multiplication between mat1 and mat2."""
+    
+    # Check if matrices can be multiplied
+    if len(mat1[0]) != len(mat2):
         return None
 
-    result = [[sum(a * b for a, b in zip(row, col)) 
-    for col in zip(*mat2)] for row in mat1]
+    # Compute the product of matrices
+    result = [
+        [sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)]
+        for row in mat1
+    ]
 
     return result
